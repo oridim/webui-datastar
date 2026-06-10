c0289903f92b2f5a83fae2f2799343ac4d41a845
oridim <125223080+oridim@users.noreply.github.com>
2026-06-10 03:27:47 -0400

    refactor: turned framework suib-module into a wrapper around `Deno.serve`

--- lib/framework.tsx
+++ lib/framework.tsx
@@ -1,65 +1,90 @@
-import type { WebUI } from '@webui/deno-webui';
-
-import type { ActionRegistry } from './actions/action-registry.ts';
 import {
-    defineActionRegistry,
-    initActions,
-} from './actions/action-registry.ts';
-
-import {
-    initIntegrations,
     INTEGRATIONS_GROUP,
     IntegrationsHead,
 } from './integrations/datastar.tsx';
 import { POLYFILLS_GROUP, PolyfillsHead } from './integrations/polyfills.tsx';
-import { WebUIHead } from './integrations/webui.tsx';
 
 import type { JSX } from './preact/components.ts';
 
 import { defineGroup } from './router/directives.ts';
-import { defineRouter, initRouter } from './router/router.ts';
 import type { Router } from './router/router.ts';
+import { defineRouter, matchRoute } from './router/router.ts';
 import type { RouteItem } from './router/types.ts';
 
-export interface WebUIDatastarOptions {
-    readonly actions?: ActionRegistry;
+import { HTTP_STATUS, HTTP_STATUS_TEXT } from './utilities/http.ts';
 
+const RESPONSE_NOT_FOUND = new Response(
+    `<h1>${HTTP_STATUS.notFound} - ${HTTP_STATUS_TEXT.notFound}</h1>`,
+    {
+        status: HTTP_STATUS.notFound,
+        headers: { 'Content-Type': 'text/html' },
+    },
+);
+
+export interface BaseServeOptions {
     readonly router?: Router;
-
-    readonly window: WebUI;
 }
 
-export function defineWebUIDatastarRouter(items: RouteItem[]): Router {
+export interface TCPServeOptions extends BaseServeOptions {
+    readonly serve?:
+        | Deno.ServeTcpOptions
+        | (Deno.ServeTcpOptions & Deno.TlsCertifiedKeyPem);
+}
+
+export interface UnixServeOptions extends BaseServeOptions {
+    readonly serve: Deno.ServeUnixOptions;
+}
+
+export interface VSockServeOptions extends BaseServeOptions {
+    readonly serve: Deno.ServeVsockOptions;
+}
+
+export function defineFrameworkRouter(items: RouteItem[]): Router {
     return defineRouter([
-        defineGroup('/__webui-datastar', [
+        defineGroup('/__datastar-deno-server', [
             ...POLYFILLS_GROUP,
             ...INTEGRATIONS_GROUP,
         ]),
+
         ...items,
     ]);
 }
 
-export function WebUIDatastarHead(): JSX.Element {
+export function FrameworkHead(): JSX.Element {
     return (
         <>
             <PolyfillsHead />
-            <WebUIHead />
             <IntegrationsHead />
         </>
     );
 }
 
-export function initWebUIDatastar(options: WebUIDatastarOptions) {
-    const {
-        actions = defineActionRegistry(),
-        router,
-        window,
-    } = options;
+export function serve(
+    options: TCPServeOptions,
+): Deno.HttpServer<Deno.NetAddr>;
+export function serve(
+    options: UnixServeOptions,
+): Deno.HttpServer<Deno.UnixAddr>;
+export function serve(
+    options: VSockServeOptions,
+): Deno.HttpServer<Deno.VsockAddr>;
+export function serve(
+    options:
+        | TCPServeOptions
+        | UnixServeOptions
+        | VSockServeOptions,
+): Deno.HttpServer<Deno.Addr> {
+    const { router, serve: serveOptions = {} } = options;
 
-    if (router) {
-        initRouter(window, router);
-    }
+    return Deno.serve(serveOptions as Deno.ServeOptions, async (request) => {
+        if (router) {
+            const response = await matchRoute(router, request);
 
-    initIntegrations(window, actions, router);
-    initActions(window, actions);
+            if (response) {
+                return response;
+            }
+        }
+
+        return RESPONSE_NOT_FOUND.clone();
+    });
 }
