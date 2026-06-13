997a5d9936790fda7fde6ed8765759902c5547bd
oridim <125223080+oridim@users.noreply.github.com>
2026-06-13 06:41:06 -0400

    chore: improved naming for middleware generics

--- lib/utilities/middleware.ts
+++ lib/utilities/middleware.ts
@@ -1,17 +1,18 @@
 import type { AnyFunction } from './types.ts';
 
-export type Middleware<T extends AnyFunction = AnyFunction> = (
-    callback: T,
-) => T;
+export type Middleware<Callback extends AnyFunction = AnyFunction> = (
+    callback: Callback,
+) => Callback;
 
-export type MiddlewareFactory<T extends AnyFunction = AnyFunction> = (
-    ...args: unknown[]
-) => Middleware<T>;
+export type MiddlewareFactory<
+    Callback extends AnyFunction = AnyFunction,
+    Args extends unknown[] = unknown[],
+> = (...args: Args) => Middleware<Callback>;
 
-export function withMiddleware<T extends AnyFunction = AnyFunction>(
-    middlewares: readonly Middleware<T>[],
-    callback: T,
-): T {
+export function withMiddleware<Callback extends AnyFunction = AnyFunction>(
+    middlewares: readonly Middleware<Callback>[],
+    callback: Callback,
+): Callback {
     return middlewares.reduceRight(
         (accumulatedCallback, middleware) => middleware(accumulatedCallback),
         callback,
